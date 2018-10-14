b6599420fca16ecdabb94f4e1f354d9cca6120b6
Jonas Aleshkevich <j.aleszkiewicz@gmail.com>
2018-10-14 14:25:17 +0200

    Nawiasy kwadratowe kontratakują: 100p

--- stas-2018/nawiasy-kwadratowe-kontratakuja.cpp
+++ stas-2018/nawiasy-kwadratowe-kontratakuja.cpp
@@ -2,61 +2,73 @@
 
 using namespace std;
 
-typedef intmax_t I;
-const I inf = 1e18;
+typedef int32_t I;
+const I inf = 1e9;
 
 struct Tree {
   I w;
-  vector<I> data, leftover;
+  vector<I> value, for_later;
   Tree(I n) {
     for (w = 1; w < n;) w *= 2;
-    data.assign(2 * w, 0);
-    leftover.assign(2 * w, 0);
+    value.assign(2 * w, 0);
+    for_later.assign(2 * w, 0);
   }
 
   I query(I qb, I qe, I rb, I re, I i, I x = 0) {
     if (re < qb || rb > qe) return inf;
     if (qb <= rb && re <= qe) {
-      data[i] += x;
-      leftover[i] += x;
-      return data[i];
+      value[i] += x;
+      for_later[i] += x;
+      return value[i];
     }
 
-    I mid = (rb + re) / 2;
+    value[2 * i] += for_later[i];
+    for_later[2 * i] += for_later[i];
 
-    data[2 * i] += leftover[i];
-    data[2 * i + 1] += leftover[i];
-    leftover[i] = 0;
+    value[2 * i + 1] += for_later[i];
+    for_later[2 * i + 1] += for_later[i];
+
+    for_later[i] = 0;
+
+    I mid = (rb + re) / 2;
 
     I r = min(query(qb, qe, rb, mid, 2 * i, x),
               query(qb, qe, mid + 1, re, 2 * i + 1, x));
 
-    data[i] = min(data[2 * i], data[2 * i + 1]);
-
+    value[i] = min(value[2 * i], value[2 * i + 1]);
     return r;
   }
+  I query(I qb, I qe, I x = 0) { return query(qb, qe, 0, w - 1, 1, x); }
 };
 
 int main() {
-  // ios_base::sync_with_stdio(false);
-  // cin.tie(nullptr);
-  // cout.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
 
-  Tree t(20);
+  I n;
+  cin >> n;
+
+  vector<I> values(n);
+
+  Tree tree(n);
+  for (I i = 0; i < n; ++i) {
+    cin >> values[i];
+    tree.query(i, n - 1, values[i]);
+  }
 
   for (;;) {
-    char a;
-    I b, c;
-    cin >> a >> b >> c;
-    if (a == '+') {
-      I d;
-      cin >> d;
-      t.query(b, c, 0, t.w - 1, 1, d);
+    I p;
+    cin >> p;
+    if (p == -1) break;
+    if (p == 0) {
+      bool ok = (tree.query(0, n - 1) >= 0 && tree.query(n - 1, n - 1) == 0);
+      cout << (ok ? "TAK" : "NIE") << '\n';
     } else {
-      cout << t.query(b, c, 0, t.w - 1, 1) << endl;
+      --p;
+      tree.query(p, n - 1, -values[p] * 2);
+      values[p] *= -1;
     }
-    for (I i = 0; i <= 20; ++i) cout << t.query(i, i, 0, t.w - 1, 1) << ' ';
-    cout << endl;
   }
 
 #ifdef __MINGW32__
