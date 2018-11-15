6c4139d486635e2ccd85aa2afe18c2f01a042700
Jonas Aleshkevich <j.aleszkiewicz@gmail.com>
2018-11-15 21:44:52 +0100

    Share the news of success with the machine

---
+++ stas-2018/zrobione.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef intmax_t I;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  cout << "Zrobione" << '\n';
+
+#ifdef UNITEST
+  cout.flush();
+  system("pause");
+#endif
+  return 0;
+}
