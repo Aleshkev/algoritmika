33b91930bc8ef06a133c2f60d6471a1f5f3673a6
Jonas Aleshkevich <j.aleszkiewicz@gmail.com>
2019-01-17 18:53:31 +0100

    Gąsienica Marianna: ups, wysłałem razem z komentarzami

--- matex-k18-a-js/gasienica-marianna.cpp
+++ matex-k18-a-js/gasienica-marianna.cpp
@@ -27,29 +27,15 @@ int main() {
   for (I &i : v) cin >> i;
 
   auto is_ok = [&](I x) {
-    // cout << "is_ok(" << x << ")" << endl;
     I hi = 0, rem = 0, sum = 0;
     for (I lo = 0; lo < n; ++lo) {
       if (lo > 0) rem -= !v[lo - 1], sum -= v[lo - 1];
       while (hi < n && rem < x) rem += !v[hi], sum += v[hi], ++hi;
       while (hi < n && v[hi]) sum += v[hi], ++hi;
-      // cout << lo << ": " << hi << " -> " << sum << endl;
       if (sum >= k) return true;
     }
     return false;
   };
-
-  // is_ok(1);
-
-  // for (I i = 0; i < n; ++i) {
-  //   // cout << i << ": " << is_ok(i) << '\n';
-  //   // cout << "checking " << i << endl;
-  //   if (is_ok(i)) {
-  //     cout << i << '\n';
-  //     break;
-  //   }
-  // }
-  // if (!is_ok(n)) cout << "NIE" << '\n';
 
   I x = f_lower_bound(0, n + 1, [&](I i) { return !is_ok(i); });
   if (x <= n)
