d407d636237f445fd43dc782c0d852c28b8baa50
Jonas Aleshkevich <j.aleszkiewicz@gmail.com>
2019-01-17 15:35:22 +0100

    Split template into 'basic' and 'extended'
    Let competition drive innovation.

---
+++ cpp-template-extended.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef intmax_t I;
+const I inf = 1e18;
+typedef pair<I, I> II;
+typedef double F;
+
+template <typename T>
+ostream &operator<<(ostream &o, const vector<T> &v) {
+  o << "[";
+  for (auto i = v.begin(); i != v.end(); ++i) {
+    cout << *i;
+    if (i != prev(v.end())) cout << ", ";
+  }
+  cout << "]";
+  return o;
+}
+
+int main() {
+  // ios_base::sync_with_stdio(false);
+  // cin.tie(nullptr), cout.tie(nullptr);
+
+  I n;
+
+#ifdef UNITEST
+  cout.flush();
+  system("pause");
+#endif
+  return 0;
+}
