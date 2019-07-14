642b902547bee011697a067a54207bc1e25bdbba
Aleshkev <j.aleszkiewicz@gmail.com>
2019-07-14 21:38:06 +0200

    Zadanie próbne na szybko: 100p

---
+++ talent-bukowina-2019/zadanie-probne-na-szybko.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef intmax_t I;
+
+int main() {
+  I a, b;
+  cin >> a >> b;
+  cout << a + b << '\n';
+
+#ifdef UNITEST
+  cout.flush();
+  system("pause");
+#endif
+  return 0;
+}
