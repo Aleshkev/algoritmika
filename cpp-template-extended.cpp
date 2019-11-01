8b005b1caa3f97857696bfdaa47129c30a44c901
Jonasz Aleszkiewicz <j.aleszkiewicz@gmail.com>
2019-11-01 19:47:45 +0100

    Compress C++ template

--- cpp-template-extended.cpp
+++ cpp-template-extended.cpp
@@ -5,32 +5,25 @@ const I inf = 1e18;
 typedef pair<I, I> II;
 typedef double F;
 
-template <typename T>
-ostream &operator<<(ostream &o, const vector<T> &v) {
-  o << "[";
-  for (auto i = v.begin(); i != v.end(); ++i) {
-    if (i != v.begin()) o << ", ";
-    o << *i;
+#define debug_printable(template_thingies, type, start, printer, stop) \
+  template <template_thingies>                                         \
+  ostream &operator<<(ostream &o, const type &v) {                     \
+    for (auto i = (o << start, v.begin()); i != v.end(); ++i)          \
+      (i != v.begin() && (o << ", ")), o << printer;                   \
+    return o << stop;                                                  \
   }
-  return o << "]";
-}
-template <typename T, typename U>
-ostream &operator<<(ostream &o, const map<T, U> &v) {
-  o << "{";
-  for (auto i = v.begin(); i != v.end(); ++i) {
-    if (i != v.begin()) o << ", ";
-    o << i->first << ": " << i->second;
-  }
-  return o << "}";
-}
+#define comma ,
+debug_printable(typename T, vector<T>, "[", *i, "]");
+debug_printable(typename T, set<T>, "{", *i, "}");
+debug_printable(typename T comma typename U, map<T comma U>, "{",
+                i->first << ": " << i->second, "}");
 template <typename T, typename U>
 ostream &operator<<(ostream &o, pair<T, U> p) {
   return o << "(" << p.first << ", " << p.second << ")";
 }
 
 int main() {
-  // ios_base::sync_with_stdio(false);
-  // cin.tie(nullptr), cout.tie(nullptr);
+  // ios_base::sync_with_stdio(false), cin.tie(nullptr);
 
   I n;
 
